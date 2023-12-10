3c2ca1a6e0b0892a577a461577bdb3210e1eeeef
pb-wd027 <104575278+pb-wd027@users.noreply.github.com>
2023-12-10 19:52:54 +0100

    Modify server.js for production and add .gitignore

--- server.js
+++ server.js
@@ -8,13 +8,17 @@ const adminRoute = require("./routes/adminRoute");
 const doctorRoute = require("./routes/doctorsRoute");
 const path = require("path");
 
-
 app.use("/api/user", userRoute);
 app.use("/api/admin", adminRoute);
 app.use("/api/doctor", doctorRoute);
 
 const port = process.env.PORT || 5000;
 
-  
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+  });
+}
+
 app.listen(port, () => console.log(`Node Express Server Started at ${port}!`));
-  
